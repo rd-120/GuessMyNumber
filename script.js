4d6c1ac2a9726839ac3f3231eae040d227c2a934
Desta <desta1202@gmail..com>
2023-01-17 16:09:16 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+'use strict';
+// document.querySelector('.message').textContent = 'correct';
+// document.querySelector('.score').textContent = 50;
+// document.querySelector('.highscore').textContent = 100;
+// document.querySelector('.guess').value = 23;
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+// document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+let highScore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  //when there is no input
+  if (!guess) {
+    displayMessage('you have to put a number 🛑');
+
+    //when guess is right player win
+  } else if (guess === secretNumber) {
+    displayMessage('correct number 💯');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    if (score > highScore) {
+      highScore = document.querySelector('.highscore').textContent = score;
+    }
+
+    //when guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too high 📉' : 'Too low 📈');
+      score--;
+      displayScore(score);
+    } else {
+      displayScore(0);
+      displayMessage('You lost the game 😥');
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayScore(20);
+  document.querySelector('.number').textContent = '?';
+  displayMessage('Start guessing...');
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+});
